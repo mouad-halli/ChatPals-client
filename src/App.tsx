4407f1b635cff9910c56f83e2aa8d98abe4adfa3
mhalli <mouadhalli1337@gmail.com>
2023-03-28 14:44:29 +0000

    initial starter commit; created react vite app; added tailwind-css

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+
+	return (
+		<div className="h-screen w-screen bg-blue-500">
+			<h1 className='text-5xl text-white'>Hello World</h1>
+		</div>
+	)
+}
+
+export default App
